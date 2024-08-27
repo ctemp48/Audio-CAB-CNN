28dedf84ec6e8c0ee333eaac1d3278a78decf0bb
ctemp48 <cltwheels@gmail.com>
2024-08-26 21:41:45 -0400

    push

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from torch import nn
 
 #read data into dataframes and reset index because it was messed up
 test = pd.read_feather('test.feather')
@@ -15,7 +16,7 @@ val = val.reset_index(drop=True)
 #create the dataset class
 class audioDataset(Dataset):
     def __init__(self, df):
-        #we must conver the ndarray to type of float32
+        #we must convert the ndarray of type object to type of float32
         self.x = torch.from_numpy(np.vstack(df['Audio'].to_numpy()).astype(np.float32))
         self.y = torch.from_numpy(df['Country'].to_numpy())
 
@@ -32,3 +33,24 @@ val_set = audioDataset(val)
 test_dataloader = DataLoader(test_set, batch_size=128, shuffle=True)
 train_dataloader = DataLoader(train_set, batch_size=128, shuffle=True)
 val_dataloader = DataLoader(val_set, batch_size=128, shuffle=True)
+
+class audioClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv1d(in_channels=1, out_channels=16, kernel_size=4)
+        self.activation1 = nn.ReLU()
+        self.pool1 = nn.MaxPool1d(kernel_size=4, stride=2)
+        self.dropout1 = nn.Dropout(p=0.15)
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=4)
+        self.activation2 = nn.ReLU()
+        self.pool2 = nn.MaxPool1d(kernel_size=4, stride=2)
+        self.dropout2 = nn.Dropout(p=0.15)
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=32, kernel_size=10)
+        self.activation3 = nn.ReLU()
+        self.pool3 = nn.MaxPool1d(kernel_size=10, stride=5)
+        self.dropout3 = nn.Dropout(p=0.1)
+        self.conv4 = nn.Conv1d(in_channels=32, out_channels=128, kernel_size=10)
+        self.activation4 = nn.ReLU()
+        self.pool4 = nn.MaxPool1d(kernel_size=10, stride=5)
+        self.batch_norm = nn.BatchNorm1d(num_features=128)
+        
