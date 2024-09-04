b97fa4bfaed973419fc730126af2e11f2223f010
ctemp48 <cltwheels@gmail.com>
2024-09-03 22:15:42 -0400

    forward

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 from torch import nn
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 #read data into dataframes and reset index because it was messed up
 test = pd.read_feather('test.feather')
 test = test.reset_index(drop=True)
@@ -18,7 +20,9 @@ class audioDataset(Dataset):
     def __init__(self, df):
         #we must convert the ndarray of type object to type of float32
         self.x = torch.from_numpy(np.vstack(df['Audio'].to_numpy()).astype(np.float32))
+        self.x.to(device)
         self.y = torch.from_numpy(df['Country'].to_numpy())
+        self.y.to(device)
 
     def __len__(self):
         return len(self.x)
@@ -52,5 +56,25 @@ class audioClassifier(nn.Module):
         self.conv4 = nn.Conv1d(in_channels=32, out_channels=128, kernel_size=10)
         self.activation4 = nn.ReLU()
         self.pool4 = nn.MaxPool1d(kernel_size=10, stride=5)
-        self.batch_norm = nn.BatchNorm1d(num_features=128)
+        self.batch_norm = nn.BatchNorm1d(num_features=396)
+    
+    def forward(self, x):
+        x = self.activation1(self.conv1(x))
+        x = self.dropout1(self.pool1(x))
+        x = self.activation2(self.conv2(x))
+        x = self.dropout2(self.pool2(x))
+        x = self.activation3(self.conv3(x))
+        x = self.dropout3(self.pool3(x))
+        x = self.activation4(self.conv4(x))
+        x = self.pool4(x)
+        x = self.batch_norm(x)
+        return x
+
+#when we start training for each x in train loader we must reshape to size of [1, 40000]
+
+model = audioClassifier()
+pred = model(test_set[0][0].reshape(1, 40000))
+print(pred.shape)
+        
+
         
